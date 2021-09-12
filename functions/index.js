b0286eb47ba58f65a48c03eec353ccd853e20606
muzammilsharief <smuzammil2000@yahoo.com>
2021-09-12 21:15:48 +0400

    Added Functionality for All Notifications

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,213 @@
-//test
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 //Init
+//ONLY DIFFERENCE
 // var serviceAccount = require("./notificationsample-5c1f4-firebase-adminsdk-plsu7-928a0e1a10.json"); //safu account
 var serviceAccount = require("./sarakku-note-firebase-adminsdk-gh9rf-2fb3f250c9.json");//Sarakku account
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
 function Message(title, body) {
-    this.title = title;
-    this.body = body;  
+  this.title = title;
+  this.body = body;
+}
+
+//Collections
+const COMPANY_COLLECTION = "company";
+const COMPANY_RELATION_COLLECTION = "company_relation";
+const PRODUCT_COLLECTION = "product";
+const PRODUCT_STATUS_COLLECTION = "product_status";
+
+//Collection Wildcards
+const PRODUCT_COLLECTION_WILDCARD = "product/{productId}";
+
+//Product Status
+const DISCOUNT_STATUS = "Discount";
+const OUT_OF_STOCK_STATUS = "Out of Stock";
+const NEW_STOCK_STATUS = "New Stock";
+const BULK_PURCHASE_STATUS = "Bulk Purchase";
+
+const ISCUSTOMER = "isCustomer";
+const ISSUPPLIER = "isSupplier";
+
+const STATUS = "status";
+const COMPANY_ID = "companyId";
+const EQUALS = "==";
+
+//Firebase Functions
+exports.changeProductStatus = functions.firestore
+  .document(PRODUCT_COLLECTION_WILDCARD)
+  .onUpdate((change, context) => {
+    console.log("Product Update Event Triggered: START");
+
+    try {
+      const previousValue = change.before.data();
+      console.log("previousValue", " => ", previousValue);
+
+      const newValue = change.after.data();
+      console.log("newValue", " => ", newValue);
+
+      if (previousValue.status != newValue.status) {
+
+        if (newValue.customerNotification) {
+          processProductStatusForCustomer(
+            newValue.status,
+            newValue.productName,
+            newValue.companyId
+          );
+        }
+
+        if (newValue.supplierNotification) {
+          processProductStatusForSupplier(
+            newValue.status,
+            newValue.productName,
+            newValue.companyId
+          );
+        }
+      }
+    } catch (error) {
+      console.log("Product Update Event error:" + error);
+    }
+    console.log("Product Update Event Triggered: END");
+  });
+
+
+const processProductStatusForCustomer = async (
+  newStatus,
+  productName,
+  companyId
+) => {
+  console.log("In processProductStatusForCustomer: START");
+
+  const db = admin.firestore();
+
+  const allDocs = await db
+    .collection(PRODUCT_STATUS_COLLECTION)
+    .where(STATUS, EQUALS, newStatus)
+    .where(ISCUSTOMER, EQUALS, true)
+    .get();
+
+  for (doc of allDocs.docs) {
+    console.log(doc.id, " => ", doc.data());
+
+    processCompanyRelationForCustomer(newStatus, productName, companyId);
+
+  }
+
+  console.log("In processProductStatusForCustomer: END");
+};
+
+
+const processProductStatusForSupplier = async (
+  newStatus,
+  productName,
+  companyId
+) => {
+  console.log("In processProductStatusForSupplier: START");
+
+  const db = admin.firestore();
+
+  const allDocs = await db
+    .collection(PRODUCT_STATUS_COLLECTION)
+    .where(STATUS, EQUALS, newStatus)
+    .where(ISSUPPLIER, EQUALS, true)
+    .get();
+
+  for (doc of allDocs.docs) {
+    console.log(doc.id, " => ", doc.data());
+
+     processCompanyRelationForSupplier(newStatus, productName, companyId);
+
+  }
+
+  console.log("In processProductStatusForSupplier: END");
+};
+
+const processCompanyRelationForCustomer = async (
+  newStatus,
+  productName,
+  companyId
+) => {
+  console.log("In processCompanyRelation: START");
+
+  console.log("Product belongs to companyId = " + companyId);
+
+  const db = admin.firestore();
+
+  const allDocs = await db
+    .collection(COMPANY_RELATION_COLLECTION)
+    .where(COMPANY_ID, EQUALS, companyId)
+    .where(ISCUSTOMER, EQUALS, true)
+    .get();
+
+  for (doc of allDocs.docs) {
+    console.log(doc.id, " => ", doc.data());
+
+    processCompany(newStatus, productName, doc.data());
+  }
+
+  console.log("In processCompanyRelation: END");
+};
+
+const processCompanyRelationForSupplier = async (
+  newStatus,
+  productName,
+  companyId
+) => {
+  console.log("In processCompanyRelationForSupplier: START");
+
+  console.log("Product belongs to companyId = " + companyId);
+
+  const db = admin.firestore();
+
+  const allDocs = await db
+    .collection(COMPANY_RELATION_COLLECTION)
+    .where(COMPANY_ID, EQUALS, companyId)
+    .where(ISSUPPLIER, EQUALS, true)
+    .get();
+
+  for (doc of allDocs.docs) {
+    console.log(doc.id, " => ", doc.data());
+
+    processCompany(newStatus, productName, doc.data());
+  }
+
+  console.log("In processCompanyRelationForSupplier: END");
+};
+
+const processCompany = async (newStatus, productName, company_relation) => {
+  console.log("In processCompany: START");
+
+  const db = admin.firestore();
+
+  const allDocs = await db
+    .collection(COMPANY_COLLECTION)
+    .where(COMPANY_ID, EQUALS, company_relation.vendorId)
+    .get();
+
+  for (doc of allDocs.docs) {
+    console.log(doc.id, " => ", doc.data());
+
+    var message = createStatusChangeMessage(newStatus, productName);
+
+    sendNotification(doc.data().fcmToken, message.title, message.body, doc.data().companyId);
+  }
+  console.log("In processCompany: END");
+};
+
+const createNewProductAddedMessage = (productName) => {
+  var title = "New Product Alert !";
+  var body = `A new ${productName} has been added, please click to view the details`;
+
+  var myMessage = new Message(title, body);
+
+  return myMessage;
 }
 
 //Firebase Functions
-exports.createProduct = functions.firestore.document("product/{docId}").onCreate((snap, context) => {
+exports.createProduct = functions.firestore
+  .document("product/{docId}")
+  .onCreate((snap, context) => {
     console.log("New Product Added Event Triggered: START");
 
     const newValue = snap.data();
@@ -22,9 +216,8 @@ exports.createProduct = functions.firestore.document("product/{docId}").onCreate
 
     console.log("New productName = " + productName);
 
-    var message = createMessage(productName);
+    var message = createNewProductAddedMessage(productName);
 
-    // sendToMuzMobile(message.title, message.body);
     sendToAll(message.title, message.body);
 
     console.log("New Product Added Event Triggered: END");
@@ -47,17 +240,38 @@ exports.sendHttpPushNotification = functions.https.onRequest(
   }
 );
 
-function createMessage(productName) { 
+const createStatusChangeMessage = (status, productName) => {
+  var title = "";
+  var body = "";
 
-  var title =  "New Product Alert !";
-  var body = `A new ${productName} has been added, please click to view the details`;
+  switch (status) {
+    case DISCOUNT_STATUS:
+      title = "!! HURRY, HURRY, SPECIAL OFFER !!";
+      body = `The item ${productName} has special discount, please click to view the details.`;
+      break;
+
+    case OUT_OF_STOCK_STATUS:
+      title = "Out Of Stock";
+      body = `The item ${productName} is out of stock, please click to view the details.`;
+      break;
+
+    case NEW_STOCK_STATUS:
+      title = "New Stock";
+      body = `A new stock of ${productName} has arrived, please click to view the details.`;
+      break;
+
+    case BULK_PURCHASE_STATUS:
+      title = "Bulk Purchase";
+      body = `There is bulk purchase for ${productName}s, please click to view the details.`;
+      break;
+  }
 
   var myMessage = new Message(title, body);
 
   return myMessage;
 }
 
-function sendToAll(title, body) {
+const sendToAll = (title, body) => {
 
   const db = admin.firestore();
 
@@ -70,46 +284,32 @@ function sendToAll(title, body) {
         console.log(fcmToken);
 
         if (fcmToken != null && fcmToken.length > 0) {
-          sendNotification(fcmToken, title, body);
+          sendNotification(fcmToken, title, body, doc.data().companyId);
         }
       });
     });
 }
 
-function sendToMuzMobile(title, body) {
-  const s1 = "f28B911bTROgXQgKuLCBkA:APA91bHEtIoFWfK3rx56kyD9TVmtDt";
-  const s2 = "-aTNZWCbhTgG_vhXKJuMWE42xDoqklLPGthhS1rxd2IKWfC5D";
-  const s3 = "obSoLrdiYnbCnLRBiiRxAKah16NkJlt";
-  const s4 = "y0EWvbsjUYIG7aSY6Bp4Aza2TFpdLc";
-  const fcmToken = s1 + s2 + s3 + s4;
+const sendNotification = async (fcmToken, title, body, companyId) => {
 
-  sendNotification(fcmToken, title, body);
-}
+  var message = `Sending notifications to ${companyId}, title = ${title} and body = ${body}`;
 
-function sendNotification(fcmToken, title, body) {
-  console.log("In sendNotification: START");
+  console.log(message);
 
-  const payload = {
-    notification: {
-      title: title,
-      body: body,
-    },
-  };
+  try {
+    const payload = {
+      notification: {
+        title: title,
+        body: body,
+      },
+    };
 
-  admin
-    .messaging()
-    .sendToDevice(fcmToken, payload)
-    .then((response) => {
-      // Response is a message ID string.
-      console.log("Successfully sent message:", response);
+    await admin.messaging().sendToDevice(fcmToken, payload);
+  } catch (error) {
+    console.log("In sendHttpPushNotification: caught exception " + error);
 
-      console.log(response.results[0].error);
+    return { error: error.code };
+  }
 
-      return { success: true };
-    })
-    .catch((error) => {
-      console.log("In sendHttpPushNotification: caught exception " + error);
-
-      return { error: error.code };
-    });
-}
+  return { success: true };
+};
